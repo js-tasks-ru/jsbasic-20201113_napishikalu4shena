058c5aff0ef1e0dbedc9ccd058f97b9be95c3a8f
Ilya Galuzinskiy <napishikalu4shena@gmail.com>
2020-11-29 18:42:21 +0300

    3rd lesson'

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -3,5 +3,7 @@
  * @returns {string}
  */
 function camelize(str) {
-  // ваш код...
+  let arr = str.split('-');
+   return arr.map((item, index) => index > 0 ? item[0].toUpperCase() + item.slice(1) : item ).join('')
+
 }
