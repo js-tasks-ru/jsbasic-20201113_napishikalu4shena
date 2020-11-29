cb0a3f5e4360aac8fa0ebb6169aae340b1e168a8
Ilya Galuzinskiy <napishikalu4shena@gmail.com>
2020-11-29 20:02:54 +0300

    3rd lesson

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -5,18 +5,9 @@
  */
 function getMinMax(str) {
   let filter = str.split(',').join(' ').split(' ')
-console.log(filter)
-  let filter2 = filter.filter((item) => Number(item)).map((item) => Number(item));
-  console.log(filter2);
-  console.log(filter2.map((item) => typeof item));
+  let filter2 = filter.filter((item) => Number(item));
 return result = {
   min: Math.min(...filter2),
   max: Math.max(...filter2)
 }
 }
-
-
-
-const inputData = '1, -5.8 или 10, хотя 34 + -5.3 и 73'
-
-console.log(getMinMax(inputData));
