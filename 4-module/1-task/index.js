60f1edff1d08b20d169957d40c4107a2ca9784d6
Ilya Galuzinskiy <napishikalu4shena@gmail.com>
2020-12-07 18:55:19 +0300

    4th module

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -4,5 +4,13 @@
  * @return {HTMLUListElement}
  */
 function makeFriendsList(friends) {
-  // ваш код...
+let uList = document.createElement('ul');
+let friendsFaS = friends.map(item => `${item.firstName} ${item.lastName}`)
+for(let i = 0; i < friends.length; i++) {
+    let li = document.createElement('li');
+    li.textContent = friendsFaS[i];
+    uList.append(li);}
+   return uList;
 }
+ 
+
--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -3,4 +3,47 @@
  * @param {Element} table
  */
 function highlight(table) {
-}
+    let genderIndex;
+    let statusIndex;
+    let ageIndex;
+  
+  for (let i = 0; i < table.rows[0].cells.length; i++) {
+    if (table.rows[0].cells[i].textContent === "Gender") {
+      genderIndex = i
+    
+    }
+    if (table.rows[0].cells[i].textContent === 'Status') {
+      statusIndex = i;
+    
+    }
+    if (table.rows[0].cells[i].textContent === 'Age') {
+      ageIndex = i;
+    
+    }
+  }
+      let classContainer = {
+      m: 'male',
+      f: 'female',
+      true: 'available',
+      false: 'unavailable'
+    }
+  
+    for (let k = 1; k < table.rows.length; k++) {
+      let classGender = table.rows[k].cells[genderIndex];
+      classGender.parentNode.classList.add(classContainer[classGender.innerHTML]);
+      let classAge = table.rows[k].cells[ageIndex].innerHTML; 
+        if (classAge < 18) {
+        classGender.parentNode.setAttribute('style', 'text-decoration: line-through')
+        }
+      let classStatus = table.rows[k].cells[statusIndex];
+        if (classStatus.hasAttribute('data-available')) {
+        let elem = classStatus.getAttribute('data-available');
+        classStatus.parentNode.classList.add(classContainer[elem]);
+      } else {
+        classStatus.parentNode.hidden = true;
+      }
+    }}
+  
+
+
+
