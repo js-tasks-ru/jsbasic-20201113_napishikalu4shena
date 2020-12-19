5f39acdb1fce631af1ee2f664bf7400035f4c30e
Ilya Galuzinskiy <napishikalu4shena@gmail.com>
2020-12-19 22:32:44 +0300

    6th lesson

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -30,5 +30,40 @@
  */
 export default class UserTable {
   constructor(rows) {
+    this.data = rows;
+    this.elem = this.render();
+      }
+  render() {
+    this.table = document.createElement('TABLE');
+    const thead = `<thead>
+    <tr>
+      <th>Имя</th>
+      <th>Возраст</th>
+      <th>Зарплата</th>
+      <th>Город</th>
+      <th></th>
+    </tr>
+  </thead>`;
+  this.table.insertAdjacentHTML('beforeend', thead);
+  const tBody = document.createElement('TBODY');
+    for (const row of this.data){
+    const rowData = `<tr>
+          <td>${row.name}</td>
+          <td>${row.age}</td>
+          <td>${row.salary}</td>
+          <td>${row.city}</td>
+          <td><button>X</button></td>
+        </tr>`;
+        tBody.insertAdjacentHTML('beforeend', rowData);
+    }
+        this.table.append(tBody);
+        
+        this.table.addEventListener('click', function(event){
+          if(event.target.closest("BUTTON")){
+            event.target.closest("TR").remove()
   }
-}
+  
+  });
+  return this.table;
+  
+}}
